567c7c3bb2fff7c5215ad027511bb426afcd5937
LearnDevAkshay <42416473+LearnDevAkshay@users.noreply.github.com>
2023-02-04 21:59:50 +0530

    Add all the code

--- src/startPratice.java
+++ src/startPratice.java
@@ -1,4 +1,36 @@
 
 public class startPratice {
+	/* 
+	 *  
+	 * @Author Akshay Fulzade
+	 * @Date 04 Feb 2023
+	 * 
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+		startPratice strP = new startPratice();
+		
+		strP.ListOfPrograms();
+		
+		
+		
+		
+	}
+	
+	
+	public void ListOfPrograms() {
+		
+		System.out.println(""
+				+ "1. Fibonaci Series"
+				+ "2. Palindrome Number Check"
+				+ "");
+		
+		
+	}
+	
+	
 
 }
